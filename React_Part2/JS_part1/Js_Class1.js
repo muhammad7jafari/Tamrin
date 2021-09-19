a38b72d5244e2ee0507280c6dc5f61974676a2dc
muhammad7jafari <m7jafari@gmail.com>
2021-09-19 21:06:35 +0430

    first

---
+++ React_Part2/JS_part1/Js_Class1.js
@@ -0,0 +1,16 @@
+const factor= -5.5;
+let firstName='Mohammad jafari';
+let isActive= false;
+let lastName= undefined;
+let age=20;
+
+age=null;
+
+//console.log(typeof factor);
+console.log(typeof firstName);
+console.log(typeof isActive);
+console.log(typeof lastName);
+console.log(typeof age);
+
+
+
